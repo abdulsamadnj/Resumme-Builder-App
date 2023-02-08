415635392adaab74fe128e467688dc95df5f9062
abdulsamadnj <122588104+abdulsamadnj@users.noreply.github.com>
2023-02-08 11:57:04 +0530

    Add files via upload

---
+++ src/constants/questionsArray.js
@@ -0,0 +1,43 @@
+let questionsArray = [
+  {
+    question: " your name?",
+    resumeField: "Name",
+    resumeFieldId: "name",
+  },
+  {
+    question: "Email address",
+    resumeField: "Email",
+    resumeFieldId: "email",
+  },
+  {
+    question: "Phone number",
+    resumeField: "Phone Number",
+    resumeFieldId: "phoneNumber",
+  },
+  {
+    question: " your address",
+    resumeField: "Adress",
+    resumeFieldId: "address",
+  },
+  {
+    question: " your career objective",
+    resumeField: "Career Objective",
+    resumeFieldId: "objective",
+  },
+  {
+    question: " your skills and abilities",
+    resumeField: "Skills",
+    resumeFieldId: "skills",
+  },
+  {
+    question: "Work experience",
+    resumeField: "Work Experience",
+    resumeFieldId: "experience",
+  },
+  {
+    question: "Education",
+    resumeField: "Educational Qualifications",
+    resumeFieldId: "education",
+  },
+];
+export default questionsArray;
